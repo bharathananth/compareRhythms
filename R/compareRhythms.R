8f1cfa44d5d083d32bcf886a1b70f0e4bc9ef9a8
Bharath Air <bharath.ananthsub@gmail.com>
2020-10-18 21:24:15 +0200

    Removed option of giving SummarizedExperiment data inputs for simplicity. Maybe add this later.

--- R/compareRhythms.R
+++ R/compareRhythms.R
@@ -1,16 +1,16 @@
 #' Run differential rhythmicity analysis
-#' @param object A matrix of log2 expression values (if microarray), expression
-#'   counts (RNA-seq) or a \linkS4class{SummarizedExperiment} object.
+#' @param data A matrix of log2 expression values (if microarray), expression
+#'   counts (RNA-seq) or normalized data (see Details).
 #' @param exp_design A data.frame of the experimental design with at least two
-#'   columns: "time" and "group". Ignored if object is a
-#'   \code{SummarizedExperiment} object.
+#'   columns: "time" and "group".
 #' @param lengths A data.frame of average transcript lengths. Only used with
 #'   methods "deseq" and "edgeR".
 #' @param method The method of analysis. It should be one of "mod_sel" for model
 #'   selection, "dodr" for analysis using \code{\link[DODR]{dodr}}, "limma" for
 #'   linear-modeling approach based on \pkg{limma}, "voom" for linear-modeling
-#'   approah for RNA-Seq using \code{\link[limma]{voom}}, "deseq" for RNA-seq
-#'   analysis using DESeq2, and "edgeR" for RNA-seq analysis using edgeR.
+#'   approah for RNA-Seq using \code{\link[limma]{voom}}, "deseq2" for RNA-seq
+#'   analysis using \pkg{DESeq2}, and "edger" for RNA-seq analysis using
+#'   \pkg{edgeR}.
 #' @param period The period of rhythm being tested (default = 24)
 #' @param rhythm_fdr The false discovery cutoff for finding rhythmic time series
 #'   (default = 0.05)
@@ -33,57 +33,31 @@
 #'   sample to account for outliers. Only used by method = "voom".
 #'
 #' @export
-compareRhythms <- function(object, exp_design=NULL, lengths=NULL,
+compareRhythms <- function(data, exp_design, lengths=NULL,
                            method = "mod_sel", period=24, rhythm_fdr = 0.05,
                            compare_fdr = 0.05, amp_cutoff = 0.5,
                            criterion = "bic", schwarz_wt_cutoff = 0.6,
-                           just_classify = TRUE, robust = TRUE, outliers = FALSE,
-                           rna_seq = FALSE) {
-
-  if (is.null(exp_design)) {
-    assertthat::assert_that(
-      attr(object, "class") %in% c("SummarizedExperiment", "RangedSummarizedExperiment"),
-      msg = "Object does not belong to SummarizedExperiment.")
-    assertthat::assert_that(
-      assertthat::not_empty(object),
-      assertthat::has_name(SummarizedExperiment::colData(object), c("time", "group")),
-      assertthat::noNA(SummarizedExperiment::assay(object, 1)),
-      is.factor(object$group),
-      is.numeric(object$time),
-      length(levels(object$group)) == 2
-    )
-    assertthat::assert_that(length(unique(SummarizedExperiment::colData(object)$group)) == 2,
-                            msg = "Data does not have exactly two groups")
-    expr <- SummarizedExperiment::assay(object, 1)
-    exp_design <- SummarizedExperiment::colData(object)
-    if ("lengths" %in% SummarizedExperiment::assayNames(object)) {
-      lengths <- SummarizedExperiment::assay(object, "lengths")
-    }
-  } else {
-    assertthat::assert_that(
-      is.matrix(object),
-      assertthat::not_empty(object),
-      is.data.frame(exp_design),
-      assertthat::not_empty(exp_design),
-      assertthat::are_equal(ncol(object), nrow(exp_design)),
-      assertthat::has_name(exp_design, c("time", "group")),
-      assertthat::noNA(object),
-      is.factor(exp_design$group),
-      is.numeric(exp_design$time),
-      length(levels(exp_design$group)) == 2
-    )
-    if (method %in% c("deseq", "edger") && !is.null(lengths)) {
-      assertthat::assert_that(all(lengths>0), msg = "All transcript lengths are not positive")
-    }
-    expr <- object
-  }
+                           just_classify = TRUE, robust = TRUE, outliers = FALSE
+                           ) {
 
   assertthat::assert_that(
+    is.matrix(data),
+    assertthat::not_empty(data),
+    is.data.frame(exp_design),
+    assertthat::not_empty(exp_design),
+    assertthat::are_equal(ncol(data), nrow(exp_design)),
+    assertthat::has_name(exp_design, c("time", "group")),
+    assertthat::noNA(data),
+    method %in% c("mod_sel", "limma", "dodr", "voom", "deseq2", "edger"),
+    is.factor(exp_design$group),
+    is.numeric(exp_design$time),
+    length(levels(exp_design$group)) == 2,
     assertthat::is.number(period),
+    period > 0,
     assertthat::is.number(rhythm_fdr),
-    rhythm_fdr <= 1.0 & rhythm_fdr > 0,
+    rhythm_fdr <= 1.0 & rhythm_fdr >= 0,
     assertthat::is.number(compare_fdr),
-    compare_fdr <= 1.0 & compare_fdr > 0,
+    compare_fdr <= 1.0 & compare_fdr >= 0,
     assertthat::is.number(amp_cutoff),
     amp_cutoff >= 0,
     assertthat::is.string(criterion),
@@ -94,32 +68,39 @@ compareRhythms <- function(object, exp_design=NULL, lengths=NULL,
     assertthat::is.flag(robust),
     assertthat::is.flag(outliers)
   )
+  if (method %in% c("deseq", "edger") && !is.null(lengths)) {
+    assertthat::assert_that(all(lengths>0), msg = "All transcript lengths are not positive")
+  }
+
+  if (assertthat::has_name(exp_design, "batch")) {
+    assertthat::assert_that(is.factor(exp_design$batch))
+  }
 
   switch (method,
-          mod_sel = compareRhythms_model_select(expr = expr,
+          mod_sel = compareRhythms_model_select(data = data,
                                                 exp_design = exp_design,
                                                 period = period,
                                                 amp_cutoff = amp_cutoff,
                                                 criterion = criterion,
                                                 schwarz_wt_cutoff = schwarz_wt_cutoff,
                                                 just_classify = just_classify),
-          dodr = compareRhythms_dodr(expr = expr,
+          dodr = compareRhythms_dodr(expr = data,
                                      exp_design = exp_design,
                                      period = period,
                                      rhythm_fdr = rhythm_fdr,
                                      compare_fdr = compare_fdr,
                                      amp_cutoff = amp_cutoff,
                                      just_classify = just_classify),
-          limma = compareRhythms_limma(eset = expr,
+          limma = compareRhythms_limma(eset = data,
                                        exp_design = exp_design,
                                        period = period,
                                        rhythm_fdr = rhythm_fdr,
                                        amp_cutoff = amp_cutoff,
                                        compare_fdr = compare_fdr,
                                        just_classify = just_classify,
-                                       rna_seq = rna_seq,
+                                       rna_seq = FALSE,
                                        robust = robust),
-          voom = compareRhythms_voom(counts = expr,
+          voom = compareRhythms_voom(counts = data,
                                      exp_design = exp_design,
                                      period = period,
                                      rhythm_fdr = rhythm_fdr,
@@ -128,13 +109,13 @@ compareRhythms <- function(object, exp_design=NULL, lengths=NULL,
                                      just_classify = just_classify,
                                      robust = robust,
                                      outliers = outliers),
-          deseq = compareRhythms_deseq2(counts = expr, exp_design = exp_design,
+          deseq2 = compareRhythms_deseq2(counts = data, exp_design = exp_design,
                                         lengths = lengths, period = period, rhythm_fdr = rhythm_fdr,
                                         amp_cutoff = amp_cutoff, compare_fdr = compare_fdr,
                                         just_classify = just_classify),
-          edger = compareRhythms_edger(counts = expr, exp_design = exp_design,
-                                        lengths = lengths, period = period, rhythm_fdr = rhythm_fdr,
-                                        amp_cutoff = amp_cutoff, compare_fdr = compare_fdr,
-                                        just_classify = just_classify)
+          edger = compareRhythms_edgeR(counts = data, exp_design = exp_design,
+                                       lengths = lengths, period = period, rhythm_fdr = rhythm_fdr,
+                                       amp_cutoff = amp_cutoff, compare_fdr = compare_fdr,
+                                       just_classify = just_classify)
   )
 }
